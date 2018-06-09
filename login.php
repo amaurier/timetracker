dc4286f4ae4b0e9947c122bd5faff1ac534c2021
Antoine Maurier <antoine.maurier@gmail.com>
2018-06-09 18:10:21 +0200

    Bootstrap 4 Add login form css classes

--- login.php
+++ login.php
@@ -36,10 +36,10 @@ if ($cl_login == null && $request->isGet()) $cl_login = @$_COOKIE['tt_login'];
 $cl_password = $request->getParameter('password');
 
 $form = new Form('loginForm');
-$form->addInput(array('type'=>'text','maxlength'=>'100','name'=>'login','value'=>$cl_login));
-$form->addInput(array('type'=>'password','maxlength'=>'50','name'=>'password','value'=>$cl_password));
+$form->addInput(array('type'=>'text','maxlength'=>'100','name'=>'login','value'=>$cl_login,'cssClass'=>'form-control'));
+$form->addInput(array('type'=>'password','maxlength'=>'50','name'=>'password','value'=>$cl_password,'cssClass'=>'form-control'));
 $form->addInput(array('type'=>'hidden','name'=>'browser_today','value'=>'')); // User current date, which gets filled in on btn_login click.
-$form->addInput(array('type'=>'submit','name'=>'btn_login','onclick'=>'browser_today.value=get_date()','value'=>$i18n->get('button.login'),'cssClass'=>'btn btn-primary'));
+$form->addInput(array('type'=>'submit','name'=>'btn_login','onclick'=>'browser_today.value=get_date()','value'=>$i18n->get('button.login'),'cssClass'=>'btn btn-primary btn-block'));
 
 if ($request->isPost()) {
   // Validate user input.
